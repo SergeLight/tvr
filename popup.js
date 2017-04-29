d6251b196eb2578ee18b038be4fe49c8b94c5ade
Serge Lichtveld <slichtveld@icloud.com>
2017-04-30 01:55:37 +0200

    buy

--- popup.js
+++ popup.js
@@ -6,7 +6,32 @@ $(document).ready(function(){
         "authorize": "xNPheF6PRHEJyJl"
     };
 
+    var prop = {
+        "proposal": 1,
+        "amount": "1",
+        "basis": "payout",
+        "contract_type": "CALL",
+        "currency": "USD",
+        "duration": "60",
+        "duration_unit": "s",
+        "symbol": "R_100"
 
+    };
+
+    function sendProp(){
+        ws.send(JSON.stringify(prop))
+    }
+    
+    function buy(id) {
+        
+        var buy = {
+            "buy": id,
+            "price": 100
+        };
+
+        ws.send(JSON.stringify(buy));
+
+    }
 
 
     ws.onopen = function(evt) {
@@ -15,8 +40,45 @@ $(document).ready(function(){
 
     ws.onmessage = function(msg) {
 
-        console.log(JSON.parse(msg.data));
+        var data = JSON.parse(msg.data);
+
+        console.log(data);
+
+        if( data.hasOwnProperty('error')){
+            
+            var message = "Dear TVR user: " + data.error.code + data.error.message
+            console.log(message);
+            
+        }else{
+            if (data.msg_type === 'proposal'){
+
+                if(data.proposal.id){
+                    buy(data.proposal.id)
+                }else{
+                    console.log('No proposal id');
+                }
+
+            }else{
+                console.log(data);
+            }
+
+        }
+
+
+
 
 
     };
-})
+
+
+    $(document).on('click', '.button', function () {
+        sendProp();
+    });
+
+
+
+
+
+
+
+});
