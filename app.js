7e569d785188ff349c348bb5c5851691000bd258
Serge Lichtveld <slichtveld@icloud.com>
2017-05-01 00:04:20 +0200

    functions

---
+++ app.js
@@ -0,0 +1,160 @@
+
+function sendProp(prop){
+    ws.send(JSON.stringify(prop))
+}
+
+function buy(id) {
+
+    var buy = {
+        "buy": id,
+        "price": 100
+    };
+
+    ws.send(JSON.stringify(buy));
+
+}
+
+function sell(id) {
+
+    var sell = {
+        "sell": id,
+        "price": 100
+    };
+
+    ws.send(JSON.stringify(sell));
+
+}
+
+function getProposal(name){
+
+    var ret;
+    
+    var prop = {
+        "buy5MinCall": {
+            "proposal": 1,
+            "amount": "1",
+            "basis": "payout",
+            "contract_type": "CALL",
+            "currency": "USD",
+            "duration": "60",
+            "duration_unit": "s",
+            "symbol": "R_100"
+        }
+        ,
+        "sell5MinCall":  {
+            "proposal": 1,
+            "amount": "1",
+            "basis": "payout",
+            "contract_type": "CALL",
+            "currency": "USD",
+            "duration": "60",
+            "duration_unit": "s",
+            "symbol": "R_100"
+        }
+    };
+
+
+    for (var key in prop){
+
+        if (prop.hasOwnProperty(name)){
+            ret =  prop[key];
+        }
+    }
+
+    return ret;
+}
+
+function authorize () {
+
+    var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=3534');
+
+
+    var auth = {
+        "authorize": "xNPheF6PRHEJyJl"
+    };
+
+    ws.onopen = function(evt) {
+        ws.send(JSON.stringify(auth));
+    };
+
+    
+
+
+    // ws.onmessage = function(msg) {
+    //
+    //     var data = JSON.parse(msg.data);
+    //
+    //     console.log(data);
+    //
+    //     if( data.hasOwnProperty('error')){
+    //
+    //         var message = "Dear TVR user: " + data.error.code + data.error.message
+    //         console.log(message);
+    //
+    //     }else{
+    //         if (data.msg_type === 'proposal'){
+    //
+    //             if(data.proposal.id){
+    //                 buy(data.proposal.id)
+    //             }else{
+    //                 console.log('No proposal id');
+    //             }
+    //
+    //         }else{
+    //             console.log(data);
+    //         }
+    //
+    //     }
+    // };
+
+}
+
+
+$(document).ready(function(){
+
+
+    // ws.onmessage = function(msg) {
+    //
+    //     var data = JSON.parse(msg.data);
+    //
+    //     console.log(data);
+    //
+    //     if( data.hasOwnProperty('error')){
+    //
+    //         var message = "Dear TVR user: " + data.error.code + data.error.message
+    //         console.log(message);
+    //
+    //     }else{
+    //         if (data.msg_type === 'proposal'){
+    //
+    //             if(data.proposal.id){
+    //                 buy(data.proposal.id)
+    //             }else{
+    //                 console.log('No proposal id');
+    //             }
+    //
+    //         }else{
+    //             console.log(data);
+    //         }
+    //
+    //     }
+    // };
+
+
+    $(document).on('click', '.button', function () {
+
+        authorize();
+        var prop = getProposal('buy5MinCall');
+        var propret = sendProp(prop);
+
+
+
+    });
+
+
+
+
+
+
+
+});
